cd613e8ba14e4682b49b3f73b2adb939e33994e5
AdiTrifan <adi.trif_an@yahoo.com>
2020-06-03 14:08:46 +0300

    Add files via upload

---
+++ RestServiceApp/src/main/java/com/company/RestService.java
@@ -0,0 +1,20 @@
+package com.company;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import java.util.Date;
+
+public class RestService {
+
+    @Path("rest")
+    public class RestServiceApp {
+
+        @GET
+        public Response rest(){
+            return Response.ok("<h1>" + new Date() + "</h1>").build();
+
+        }
+
+
+    }
+}
